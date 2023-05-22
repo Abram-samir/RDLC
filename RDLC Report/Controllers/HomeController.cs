151fcfee1374aad328803f0a85042e4d6d1f2dc9
abram samir <abramsamir76@gmail.com>
2023-05-22 17:19:01 +0300

    Add project files.

---
+++ RDLC Report/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using RDLC_Report.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RDLC_Report.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public void GetEmployeeReport()
+        {
+            ReportParams objReportParams = new ReportParams();
+            var data = GetEmployeeInfo();
+            objReportParams.DataSource = data.Tables[0];
+            objReportParams.ReportTitle = "Employee Info Report";
+            objReportParams.RptFileName = "";
+            objReportParams.ReportType = "EmployeeInfoReport";
+            objReportParams.DataSetName = "dsEmployeeReport";
+            this.HttpContext.Session["ReportParam"] = objReportParams;
+
+        }
+
+        private DataSet GetEmployeeInfo()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["sqlConnectionString"].ConnectionString;
+            DataSet ds = new DataSet();
+            string sql = "select * from Employee";
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+            adpt.Fill(ds);
+            return ds;
+        }
+    }
+}
---
+++ RDLC Report/Scripts/EmployeeReport.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $("#btnGenerate").click(function () {
+        ReportManger.GenerateReport();
+    });
+});
+
+var ReportManger = {
+    GenerateReport: function () {
+        var jsonParam = "";
+        var serviceUrl = "../Home/GetEmployeeReport";
+        ReportManger.GetReport(serviceUrl, jsonParam, onFailed);
+        function onFailed(error) {
+            alert(error);
+        }
+    },
+    GetReport: function (serviceUrl, jsonParams, errorCallback) {
+        jQuery.ajax({
+            url: serviceUrl,
+            async: false,
+            type: "POST",
+            data: "{" + jsonParams + "}",
+            contentType: "application/json;charset=utf-8",
+            success: function () {
+                window.open('../Reports/ReportViewer.aspx', '_newtab');
+            },
+            error:errorCallback
+        })
+    }
+};
+
+var ReprotHelper = {
+
+};
